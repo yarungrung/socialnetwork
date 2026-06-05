4f82a23bde788595ee83d2705ac8c51b7f7d65eb
yarungrung <s1243032@gm.ncue.edu.tw>
2026-06-05 06:32:26 +0000

    modified:   app.py

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-import os
-import random
-import geopandas as gpd
-import matplotlib.cm as cm
-import matplotlib.pyplot as plt
-import networkx as nx
-import numpy as np
-import osmnx as ox
-import pandas as pd
-from scipy.spatial import cKDTree
-from shapely.geometry import Polygon
+import streamlit as st
+
+# 1. 使用 st.Page() 定義所有頁面
+# 注意：st.Page() 會自動尋找 .py 檔案
+pages = [
+   st.Page("mainpage.py", title="專案首頁", icon="🏠"),
+]
+
+# 2. 使用 st.navigation() 建立導覽 (例如在側邊欄)
+with st.sidebar:
+    st.title("關於我：自我介紹")
+    # st.navigation() 會回傳被選擇的頁面
+    selected_page = st.navigation(pages)
+
+
+# 3. 執行被選擇的頁面
+selected_page.run()
