cd2fad3fd36568e966cd2d71accdcd983ec7a838
yarungrung <s1243032@gm.ncue.edu.tw>
2026-06-05 06:30:19 +0000

    new file:   app.py 	new file:   mainpage.py 	new file:   requirements.txt

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+import random
+import geopandas as gpd
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+import osmnx as ox
+import pandas as pd
+from scipy.spatial import cKDTree
+from shapely.geometry import Polygon
